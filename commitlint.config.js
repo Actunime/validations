3f74f1de0d2028e389590f578bd6f69c5d48f4d0
DevlM <dev.maudon@gmail.com>
2025-02-18 11:54:21 +0100

    fix: auto version

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        //   TODO Add Scope Enum Here
+        // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+        "type-enum": [
+            2,
+            "always",
+            [
+                "deploy",
+                "maj",
+                "correction",
+                "lint",
+                "config",
+                "feat",
+                "fix",
+                "docs",
+                "chore",
+                "style",
+                "refactor",
+                "ci",
+                "test",
+                "perf",
+                "revert",
+            ],
+        ],
+    },
+}
