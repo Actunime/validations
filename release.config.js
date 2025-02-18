95bb585cec37d37fd4b190f7683a123ab415e949
DevlM <dev.maudon@gmail.com>
2025-02-18 12:31:22 +0100

    fix: semantic-release

--- release.config.js
+++ release.config.js
@@ -13,7 +13,6 @@ module.exports = {
     verifyConditions: ['@semantic-release/github'],
     prepare: [
         '@semantic-release/changelog',
-        '@semantic-release/npm',
         {
             path: '@semantic-release/git',
             assets: ['package.json', 'CHANGELOG.md'],
