7bb03a557f99248009a50945793dbb84fbf2c7e4
DevlM <dev.maudon@gmail.com>
2025-02-18 12:30:59 +0100

    fix: semantic-release

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "branches": [
+        "main"
+    ],
+    repositoryUrl: "https://github.com/Actunime/typescript-config",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/github",
+        "@semantic-release/git"
+    ],
+    verifyConditions: ['@semantic-release/github'],
+    prepare: [
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        {
+            path: '@semantic-release/git',
+            assets: ['package.json', 'CHANGELOG.md'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+    ],
+    publish: [
+        {
+            path: '@semantic-release/github'
+        }
+    ]
+}
