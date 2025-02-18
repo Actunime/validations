2711ef278cf75f0a97f707d651a6467e4ca1e85c
DevlM <dev.maudon@gmail.com>
2025-02-18 14:44:10 +0100

    fix: template

--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as BaseConfig } from './config/base.json';
-export { default as ReactLibraryConfig } from './config/react-library.json';
-export { default as NextJsConfig } from './config/nextjs.json';
-export { default as NodeConfig } from './config/node.json';
