4e0e91149e8f6d834f5d776865f5c258ed611ab5
DevlM <dev.maudon@gmail.com>
2025-02-18 13:54:43 +0100

    fix: structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log("Hello from typescript-config");
+
+export * as BaseConfig from './base.json';
+export * as ReactLibraryConfig from './react-library.json';
+export * as NextJsConfig from './nextjs.json';
+export * as NodeConfig from './node.json';
