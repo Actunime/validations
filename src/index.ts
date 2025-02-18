61b0decc2813e7a15a64086e96262735bfa1cae5
DevlM <dev.maudon@gmail.com>
2025-02-18 14:24:31 +0100

    fix: template

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-console.log("Hello from typescript-config");
-
-export { default as BaseConfig } from './config/base.json';
-export { default as ReactLibraryConfig } from './config/react-library.json';
-export { default as NextJsConfig } from './config/nextjs.json';
-export { default as NodeConfig } from './config/node.json';
+console.log("Hello from package-x");
